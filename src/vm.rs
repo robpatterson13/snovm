0a198981b067f881822911d7dcc3a027ca891ad1
robpatterson13 <robertpatterson1313@gmail.com>
2026-05-20 16:04:06 -0400

    Fix test

--- src/vm.rs
+++ src/vm.rs
@@ -205,7 +205,8 @@ mod tests {
             Instruction::LoadC(1),
             Instruction::Add,
             Instruction::LoadC(1),
-            Instruction::Sub
+            Instruction::Sub,
+            Instruction::Halt
         ];
         let vm = VM::new(&insts);
         let result = vm.execute();
